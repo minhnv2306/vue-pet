cf18dcec513a1a93c93bde66fc2bc8c54743934a
minhnv2306 <minhnv2306@gmail.com>
2022-07-27 21:11:02 +0700

    section 08: cmp-communication-03-validating-props

--- section8/source_code/src/App.vue
+++ section8/source_code/src/App.vue
@@ -8,6 +8,7 @@
         name="Manuel Lorenz"
         phone-number="01234 78992"
         email-address="manuel@localhost.com"
+        is-favorite="1"
       ></friend-contact>
       <friend-contact
         name="Julie Jones"
