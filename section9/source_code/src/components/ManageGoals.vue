cf73c4c35baed4b42cb65f6a9303af45b448dd71
minhnv2306 <minhnv2306@gmail.com>
2022-07-27 21:55:45 +0700

    section 09: cmp-adv-09-working-with-fragments

--- section9/source_code/src/components/ManageGoals.vue
+++ section9/source_code/src/components/ManageGoals.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="setGoal">Set Goal</button>
-    <teleport to="body">
-      <error-alert v-if="inputIsInvalid">
-        <h2>Input is invalid!</h2>
-        <p>Please enter at least a few characters...</p>
-        <button @click="confirmError">Okay</button>
-      </error-alert>
-    </teleport>
-  </div>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </teleport>
 </template>
 
 <script>
@@ -18,11 +16,11 @@ import ErrorAlert from './ErrorAlert.vue';
 
 export default {
   components: {
-    ErrorAlert
+    ErrorAlert,
   },
   data() {
     return {
-      inputIsInvalid: false
+      inputIsInvalid: false,
     };
   },
   methods: {
@@ -34,7 +32,7 @@ export default {
     },
     confirmError() {
       this.inputIsInvalid = false;
-    }
-  }
-}
+    },
+  },
+};
 </script>
